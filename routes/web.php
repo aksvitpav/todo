92eae66acbeba9a417eb5667844de78831eec33e
aksvitpav <aksvitpav@google.com>
2020-04-15 11:22:10 +0300

    add welcome view, tasl migration

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');;
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('tasks', 'TaskController');
